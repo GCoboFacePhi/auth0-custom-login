9f42d1f2e3f0ee3599b577305a289aa4d5e96e44
gcobofacephi <guillermocobo@facephi.com>
2023-03-13 11:26:44 +0100

    include reset-password content

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import { Spinner } from '@facephi/ui-spinner'
+import { InphiniteThemeProvider } from '@facephi/ui-theme'
+import { ToastProvider } from '@facephi/ui-toast'
+import React, { Suspense } from 'react'
+import ReactDOM from 'react-dom/client'
+import './i18n'
+import './index.css'
+import LoginPage from './pages/Login'
+import ResetPassword from './pages/ResetPassword'
+import { AuthProvider, TranslationProvider } from './providers'
 
-const rootElement = document.getElementById('root');
-ReactDOM.createRoot(rootElement as HTMLElement).render(
+const rootElement = document.getElementById('root')
+const resetElement = document.getElementById('reset-password')
+
+let content = null
+if (rootElement) {
+  content = <LoginPage />
+} else if (resetElement) {
+  content = <ResetPassword />
+}
+
+const targetElement = rootElement || resetElement
+
+ReactDOM.createRoot(targetElement as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <InphiniteThemeProvider>
+      <TranslationProvider>
+        <AuthProvider>
+          <ToastProvider>
+            <Suspense fallback={<Spinner />}>{content}</Suspense>
+          </ToastProvider>
+        </AuthProvider>
+      </TranslationProvider>
+    </InphiniteThemeProvider>
   </React.StrictMode>,
-);
+)
